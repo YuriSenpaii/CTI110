c345777cd603ad02b56c27d92a8f2d37bd8a5609
YuriSenpaii <101838884+YuriSenpaii@users.noreply.github.com>
2026-06-08 19:23:13 -0400

    first lab
    input and output print

---
+++ P1LAB1_RamirezJunior.py
@@ -0,0 +1,12 @@
+ # Junior Ramirez
+ # 06/08/2026
+ # input_output 
+ # This program will ask the user for their name and then greet them.
+ 
+ 
+ 
+ 
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+ 
+print( "Hello," ,first_name, last_name  + "! Welcome to CTI-110" )
