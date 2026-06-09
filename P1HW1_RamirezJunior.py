9d9a464cb908f5c447fa395225c2f9c4ad387c2e
YuriSenpaii <101838884+YuriSenpaii@users.noreply.github.com>
2026-06-09 17:32:20 -0400

    P1HW1
    small calculator

---
+++ P1HW1_RamirezJunior.py
@@ -0,0 +1,33 @@
+# Junior Ramirez
+# 06/08/2026
+# P1HW1
+#  write code that collects information from user, processes information collected and display results to user.
+
+print("------Exponent------")
+print()
+
+base = int(input("Enter a base number: "))
+
+exponent = int(input("Enter an exponent: "))
+
+result = base ** exponent 
+
+print(base, "raised to the power of", exponent, "is", result, "!!")
+
+# calculate addition and subtraction 
+
+
+print("------Addition and Subtraction-----")
+print()
+
+
+numm1 = int(input("Enter a Starting number: "))
+num2 = int(input("Enter a integer to add: "))
+num3 =  int(input("Enter a integer to subtract: "))
+
+sum_result = numm1 + num2
+
+final_result = sum_result - num3
+
+print(numm1, "+", num2, "-", num3, " is equal to ", final_result, "!!")
+
